ad97a9fff2355741033c578136681716785e4e3b
guoshencheng <648772021@qq.com>
2016-01-20 10:51:54 +0800

    modify url

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
-//wx6f6c7828adb6b4a4
-//73769a30f5e2ba3114f8e5f04f19bcc8
+var config = {
+  token: 'guoshencheng',
+  appId: 'wx6f6c7828adb6b4a4',
+  encodingAESKey: 'a6A2gGxXu5lAKc2lz9njpBQa01BJRFMSChQPg6ttl8l'
+}
+
 var WechatAPI = require('wechat-api')
 var api = new WechatAPI('wx6f6c7828adb6b4a4', '73769a30f5e2ba3114f8e5f04f19bcc8')
 /* GET home page. */
@@ -13,17 +17,10 @@ router.get('/config', function(req, res, next) {
   var param = {
     debug: true,
     jsApiList: ['translateVoice', 'startRecord', 'stopRecord', 'playVoice', 'pauseVoice', 'stopVoice', 'onVoicePlayEnd', 'uploadVoice', 'downloadVoice'],
-    url: 'http://wx.easyell.com'
+    url: 'http://wx.easyell.com/config'
   }
   api.getJsConfig(param, function(err, result) {
     if (err) console.log(err)
-    //res.render('index', {
-    //  debug:result.debug,
-    //  appId: result.appId,
-    //  timestamp: result.timestamp,
-    //  nonceStr: result.nonceStr,
-    //  signature: result.signature,
-    //});
     res.render('index', {config: result})
   })
 })
