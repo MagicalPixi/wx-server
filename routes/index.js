fcdd0ff077391ef1c1a67298aafabd86f138565e
guoshencheng <648772021@qq.com>
2016-01-27 02:01:02 +0800

    wechat check

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var wechat = require('wechat')
 var config = {
   token: 'guoshencheng',
   appId: 'wx6f6c7828adb6b4a4',
@@ -7,11 +8,24 @@ var config = {
 }
 
 var WechatAPI = require('wechat-api')
+
+var config = {
+  token: 'guoshencheng',
+  appid: 'wx6f6c7828adb6b4a4',
+  encodingAESKey: 'a6A2gGxXu5lAKc2lz9njpBQa01BJRFMSChQPg6ttl8l'
+};
 var api = new WechatAPI('wx6f6c7828adb6b4a4', '73769a30f5e2ba3114f8e5f04f19bcc8')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/wechat', wechat(config, wechat.text(function(message, req, res, next) {
+  res.reply({
+    type: 'text',
+    content: '别乱玩，会出事的'
+  })
+})))
 
 router.get('/config', function(req, res, next) {
   var param = {
