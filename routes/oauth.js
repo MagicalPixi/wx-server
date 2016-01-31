17e903034ad7f0b5d8d4e0c3e6c421c7fde1eaa8
guoshencheng <648772021@qq.com>
2016-01-31 18:30:18 +0800

    change place to init monster

--- routes/oauth.js
+++ routes/oauth.js
@@ -11,9 +11,7 @@ var oauth = new OAuth(wxConfig.appid, wxConfig.secretKey, function (openid, call
     callback(null, result.token)
   })
 }, function (openid, token, callback) {
-  createOrSaveUser(openid, token, function() {
-    createOrSaveMonster(openid, callback)
-  })
+  createOrSaveUser(openid, token, callback)
 });
 
 var createOrSaveUser = function(openid, token, callback) {
@@ -21,23 +19,8 @@ var createOrSaveUser = function(openid, token, callback) {
   User.findOne({openid: openid}, function(err, result) {
     result = result || new User({openid: openid})
     result.token = token
-    result.save(function (err, result) {
-      if (callback) callback()
-    })
-  })
-}
-
-var createOrSaveMonster = function(openid, callback) {
-  var Monster = model.Monster
-  Monster.findOne({ownerid: openid}, function(err, result) {
-    result = result || new Monster({ownerid: openid, property1: 0, property2: 0, property3: 0, property4: 0, property5: 0})
-    result.type = randomType()
     result.save(callback)
   })
-}
-
-var randomType = function() {
-  return parseInt(Math.random() * 4)
 }
 
 module.exports = oauth
