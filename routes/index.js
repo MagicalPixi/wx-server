4dcd5db76a1caf13d7d09fe48711b4bef2d591f5
guoshencheng <648772021@qq.com>
2016-02-01 14:39:25 +0800

    add monster update and get function

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ var gameController = require('./gameController')
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.post('/monster/:id', gameController.updateMonster);
+router.get('/monster/:id', gameController.monster)
 router.get('/game', gameController.checkAuthorize, gameController.getGameView)
 router.post('/wechat',wechat.example)
 router.get('/wechat', wechat.check)
