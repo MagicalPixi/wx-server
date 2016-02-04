46c2c47c0768bc531e2fe8c658c9895e84d72919
guoshencheng <648772021@qq.com>
2016-02-04 23:11:21 +0800

    make sethp function for hp

--- public/sprites/playerhp/index.js
+++ public/sprites/playerhp/index.js
@@ -2,5 +2,15 @@ var mySprite = require('./sprite.js');
 mySprite.render = function () {
 }
 
-mySprite.gotoAndStop(9)
+mySprite.setHp = function(hp) {
+  this.hp = hp
+  mySprite.gotoAndStop(hp)
+}
+
+mySprite.injured = function(count) {
+  this.setHp(this.hp - count)
+}
+
+mySprite.setHp(9)
+
 module.exports = mySprite;
