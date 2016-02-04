95db2718d8b36672ddb09804f4d884c90d28d58f
guoshencheng <648772021@qq.com>
2016-02-04 23:27:48 +0800

    add end sprites

---
+++ public/sprites/playagain/sprite.js
@@ -0,0 +1,66 @@
+var lib = require('pixi-lib');
+
+var mySprite = lib.getMc({
+
+    textures:lib.getTextures('playagain'),
+
+    
+
+        
+
+        "spriteName" :  "playagain" ,
+
+        
+
+    
+
+        
+
+        "y" :  900 ,
+
+        
+
+    
+
+        
+
+        "scale.y" :  0.6 ,
+
+        
+
+    
+
+        
+
+        "animationSpeed" :  0.1 ,
+
+        
+
+    
+
+        
+
+        "rotation" :  0 ,
+
+        
+
+    
+
+        
+
+        "x" :  15 ,
+
+        
+
+    
+
+        
+
+        "scale.x" :  0.6 ,
+
+        
+
+    
+});
+
+module.exports = mySprite;
---
+++ public/sprites/share/sprite.js
@@ -0,0 +1,74 @@
+var lib = require('pixi-lib');
+
+var mySprite = lib.getMc({
+
+    textures:lib.getTextures('share'),
+
+    
+
+        
+
+        "spriteName" :  "share" ,
+
+        
+
+    
+
+        
+
+        "y" :  900 ,
+
+        
+
+    
+
+        
+
+        "anchor.x" :  1 ,
+
+        
+
+    
+
+        
+
+        "scale.y" :  0.6 ,
+
+        
+
+    
+
+        
+
+        "animationSpeed" :  0.1 ,
+
+        
+
+    
+
+        
+
+        "rotation" :  0 ,
+
+        
+
+    
+
+        
+
+        "x" :  625 ,
+
+        
+
+    
+
+        
+
+        "scale.x" :  0.6 ,
+
+        
+
+    
+});
+
+module.exports = mySprite;
