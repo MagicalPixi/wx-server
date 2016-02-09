de7152af3dd0df20c4c099510d378caa7b8134a5
guoshencheng <648772021@qq.com>
2016-02-09 22:36:36 +0800

    调整位置

--- public/js/newyear/battle/explanation.js
+++ public/js/newyear/battle/explanation.js
@@ -4,21 +4,21 @@
 
 var explanation = new PIXI.Container()
 
-var text = new PIXI.Text('1111111111111111111\n11111111111111111111111', {
+var text = new PIXI.Text('', {
   font: '30px Arial',
   fill: 0x666666,
   align: 'left'
 })
 
-text.y = 300
+text.y = 400
 text.x = 20
 
 explanation.addChild(text)
 
 var error = ['有点饿了', '卖了个萌', '身体不舒服', '春心荡漾']
 
-var names = ['赛雷', '大扫除!', '放火',
-  '尖叫', '生气', '流汗', '摇尾巴', '变圆', '眨眼', '摇一摇', '挂掉了']
+var names = ['赛雷!', '大扫除!', '放火!',
+  '尖叫!', '生气...', '流汗...', '摇尾巴', '变圆', '眨眼', '摇一摇', '挂掉了']
 
 explanation.update = function (enemy, success, index) {
   text.text = enemy ? '敌方' :'你的' + '年兽' + (success ? ('成功使用技能 \'' + names[index] + ' \'') : (error[parseInt(Math.random() * error.length)] +
