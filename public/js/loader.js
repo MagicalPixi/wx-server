924d7d0db51438584d3c70d92211fd5b96bbcd81
zhou-yg <zhou.y.g890@gmail.com>
2016-02-15 22:17:25 +0800

    qiniu cdn

--- public/js/loader.js
+++ public/js/loader.js
@@ -3,5 +3,6 @@ var pixiLib = require('pixi-lib');
 window.dispatch = pixiLib.createAction.dispatch;
 
 module.exports = pixiLib.createLoader({
-  publicPath:'/sprites/'
+  //publicPath:'http://7tszl1.com1.z0.glb.clouddn.com/wx/'
+  publicPath:'http://7u2min.com1.z0.glb.clouddn.com/wx/'
 });
