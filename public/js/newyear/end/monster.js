ea39f3fce36be4dd7b000069717cf113fc0daa9d
guoshencheng <648772021@qq.com>
2016-02-15 22:05:06 +0800

    add app message

--- public/js/newyear/end/monster.js
+++ public/js/newyear/end/monster.js
@@ -92,6 +92,17 @@ myMonster.update = function (beat) {
       // 用户取消分享后执行的回调函数
     }
   });
+  wx.onMenuShareAppMessage({
+    title: shareText, // 分享标题
+    link: 'http://wx.easyell.com/game' + window.user.opendid, // 分享链接
+    imgUrl: '', // 分享图标
+    success: function () {
+      // 用户确认分享后执行的回调函数
+    },
+    cancel: function () {
+      // 用户取消分享后执行的回调函数
+    }
+  });
 
   var mymonster = window.mymonster
   tall.gotoAndStop(mymonster.property1)
