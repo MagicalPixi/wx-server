bb7ef9cb1152388388cfc6e917deab1e6fbf849c
guoshencheng <648772021@qq.com>
2016-02-17 15:28:59 +0800

    chenge resource

--- public/js/loader.js
+++ public/js/loader.js
@@ -4,5 +4,5 @@ window.dispatch = pixiLib.createAction.dispatch;
 
 module.exports = pixiLib.createLoader({
   //publicPath:'http://7tszl1.com1.z0.glb.clouddn.com/wx/'
-  publicPath:'http://7u2min.com1.z0.glb.clouddn.com/wx/'
+  publicPath:'http://7u2min.com1.z0.glb.clouddn.com/wx2/'
 });
